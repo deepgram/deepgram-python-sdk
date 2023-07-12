d9230e45618fbc087c0d37898309480011a8c6ec
Brent George <brent.george@deepgram.com>
2023-07-12 16:44:20 -0400

    add explicit encoding to setup.py file for wheel build

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ import os.path
 
 # from official Python version-detection recommendations: https://packaging.python.org/guides/single-sourcing-package-version/
 
-with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'deepgram', '_version.py')) as file:
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'deepgram', '_version.py'), encoding="utf8") as file:
     exec(file.read())
 # imports as __version__
 
-with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md')) as file:
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'), encoding="utf8") as file:
     long_description = file.read()
 
 setuptools.setup(
