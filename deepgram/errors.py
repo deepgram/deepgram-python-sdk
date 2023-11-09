526fcb7d13f7c2624da2b120bcfefc53ddf82cdf
dvonthenen <david.vonthenen@gmail.com>
2023-11-09 10:55:47 -0800

    Restructure Python v3 (Part 6) - Add Licensing Headers

--- deepgram/errors.py
+++ deepgram/errors.py
@@ -1,3 +1,6 @@
+# Copyright 2023 Deepgram SDK contributors. All Rights Reserved.
+# Use of this source code is governed by a MIT license that can be found in the LICENSE file.
+# SPDX-License-Identifier: MIT
 
 class DeepgramError(Exception):
     """
