6f9f4464316a9fe8dbccac44dde1e07be0fd5e9c
dvonthenen <david.vonthenen@gmail.com>
2023-11-28 12:18:24 -0800

    CD Release Fixes

--- setup.py
+++ setup.py
@@ -2,39 +2,41 @@
 # Use of this source code is governed by a MIT license that can be found in the LICENSE file.
 # SPDX-License-Identifier: MIT
 
-import setuptools
+from setuptools import setup, find_packages
 import os.path
 
-with open(
-    os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.md"),
-    encoding="utf8",
-) as file:
-    long_description = file.read()
+with open("README.md", "r", encoding="utf-8") as fh:
+    LONG_DESCRIPTION = fh.read()
 
-setuptools.setup(
+DESCRIPTION = (
+    "The official Python SDK for the Deepgram automated speech recognition platform."
+)
+
+setup(
     name="deepgram-sdk",
-    description="The official Python SDK for the Deepgram automated speech recognition platform.",
-    long_description=long_description,
+    author="Deepgram",
+    author_email="devrel@deepgram.com",
+    url="https://github.com/deepgram/deepgram-python-sdk",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     license="MIT",
-    url="https://github.com/deepgram/deepgram-python-sdk",
-    author="Luca Todd",
-    author_email="luca.todd@deepgram.com",
-    classifiers=[
-        "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3",
-    ],
-    keywords="deepgram speech-to-text",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     install_requires=[
         "httpx",
         "websockets",
-        'typing-extensions; python_version < "3.8.0"',
+        "typing-extensions; python_version < '3.8.0'",
         "dataclasses-json",
         "dataclasses",
         "typing_extensions",
         "python-dotenv",
         "asyncio",
         "aiohttp",
+    ],
+    keywords=["deepgram", "deepgram speech-to-text"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
     ],
 )
