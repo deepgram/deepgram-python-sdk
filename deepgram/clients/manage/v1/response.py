4f4dca57e4b5efa8bd2334a21f841a06e0baac4c
SUNIL SINGH <sunil.hadoopdev@gmail.com>
2024-01-17 18:55:42 +0000

    modifyed response.py removed the  Enum package import as it no longer utilized anywhere issue #170

--- deepgram/clients/manage/v1/response.py
+++ deepgram/clients/manage/v1/response.py
@@ -6,7 +6,6 @@ from dataclasses import dataclass
 from dataclasses_json import dataclass_json
 from datetime import datetime
 from typing import TypedDict, List, Optional
-from enum import Enum
 
 
 # Result Message
