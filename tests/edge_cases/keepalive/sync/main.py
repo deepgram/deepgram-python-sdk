2224bb8b4d5f321cde1be321cd38806af3886682
David vonThenen <12752197+dvonthenen@users.noreply.github.com>
2024-03-26 10:32:34 -0700

    Examples to Demonstrate KeepAlive and Failure for Not Using It

---
+++ tests/edge_cases/keepalive/sync/main.py
@@ -0,0 +1,35 @@
+# Copyright 2024 Deepgram SDK contributors. All Rights Reserved.
+# Use of this source code is governed by a MIT license that can be found in the LICENSE file.
+# SPDX-License-Identifier: MIT
+
+import time
+import logging, verboselogs
+
+from deepgram import DeepgramClient, DeepgramClientOptions, LiveOptions
+
+
+def main():
+    # for debugging
+    config: DeepgramClientOptions = DeepgramClientOptions(
+        verbose=logging.DEBUG, options={"keepalive": "true"}
+    )
+    deepgram: DeepgramClient = DeepgramClient("", config)
+    # OR
+    # deepgram: DeepgramClient = DeepgramClient()
+
+    deepgram_connection = deepgram.listen.live.v("1")
+
+    deepgram_connection.start(LiveOptions())
+
+    # press any key to exit
+    input("\n\nPress Enter to exit...\n\n")
+
+    print("deadlock!")
+    try:
+        deepgram_connection.finish()
+    finally:
+        print("no deadlock...")
+
+
+if __name__ == "__main__":
+    main()
