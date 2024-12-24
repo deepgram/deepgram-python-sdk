e4b2ab72921f1ca159cfdd350c110ad248b6cf07
Gerry <gerry@hippocraticai.com>
2024-12-23 17:47:57 -0800

    address task rabbit comments

--- deepgram/clients/listen/v1/websocket/client.py
+++ deepgram/clients/listen/v1/websocket/client.py
@@ -38,10 +38,12 @@ class ListenWebSocketClient(
     """
     Client for interacting with Deepgram's live transcription services over WebSockets.
 
-     This class provides methods to establish a WebSocket connection for live transcription and handle real-time transcription events.
+    This class provides methods to establish a WebSocket connection for live transcription and handle real-time transcription events.
 
-     Args:
-         config (DeepgramClientOptions): all the options for the client.
+    Args:
+        config (DeepgramClientOptions): all the options for the client.
+        thread_cls (Type[threading.Thread]): optional thread class to use for creating threads,
+        defaults to threading.Thread. Useful for custom thread management like ContextVar support.
     """
 
     _logger: verboselogs.VerboseLogger
